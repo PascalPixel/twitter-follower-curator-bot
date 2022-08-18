cf7db654abac56aecfece664f8b0ecc2fbe898a3
Pascal Pixel <x@pascalpixel.com>
2022-08-18 02:59:27 +0100

    redo packages

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ import { writeFile } from "fs/promises";
 
 config();
 
-async function main() {
+export async function getFollowers() {
   const followers = [];
   let max_results = 1000;
 
-  async function getFollowers(next_token) {
+  async function getFollowersSet(next_token) {
     // timeout not to hammer the API
     if (next_token) await new Promise((resolve) => setTimeout(resolve, 5000));
 
     try {
       const id = process.env.TWITTER_USER_ID;
       const url = new URL(`https://api.twitter.com/2/users/${id}/followers`);
-      url.search = new URLSearchParams(next_token ? { max_results, pagination_token: next_token } : { max_results });
+      url.search = new URLSearchParams(
+        next_token
+          ? { max_results, pagination_token: next_token }
+          : { max_results }
+      );
 
       const response = await fetch(url, {
         headers: {
@@ -27,16 +31,24 @@ async function main() {
 
       followers.push(...json.data);
 
-      if (json.meta.next_token) return await getFollowers(json.meta.next_token);
+      if (json.meta.next_token)
+        return await getFollowersSet(json.meta.next_token);
       return console.log("done getting followers");
     } catch (err) {
       return console.log(err);
     }
   }
 
-  await getFollowers();
+  await getFollowersSet();
 
-  if (followers.length) writeFile("./followers.json", JSON.stringify(followers), (err) => console.error(err));
+  if (followers.length)
+    writeFile("./followers.json", JSON.stringify(followers), (err) =>
+      console.error(err)
+    );
+}
+
+export default async function main() {
+  await getFollowers();
 
   return console.log("done");
 }
