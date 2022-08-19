0f11e2d7cc98a86620ef1e1264ca3469678c13f7
Pascal Pixel <x@pascalpixel.com>
2022-08-19 12:13:17 +0100

    move to twitter-api-v2

--- main.js
+++ main.js
@@ -1,56 +1,78 @@
-import { config } from "dotenv";
-import fetch from "node-fetch";
+import { config as loadEnvVariables } from "dotenv";
 import { writeFile } from "fs/promises";
+import { TwitterApi } from "twitter-api-v2";
 
-config();
+loadEnvVariables();
 
-export async function getFollowers() {
-  const followers = [];
-  let max_results = 1000;
-
-  async function getFollowersSet(next_token) {
-    // timeout not to hammer the API
-    if (next_token) await new Promise((resolve) => setTimeout(resolve, 5000));
-
-    try {
-      const id = process.env.TWITTER_USER_ID;
-      const url = new URL(`https://api.twitter.com/2/users/${id}/followers`);
-      url.search = new URLSearchParams(
-        next_token
-          ? { max_results, pagination_token: next_token }
-          : { max_results }
-      );
-
-      const response = await fetch(url, {
-        headers: {
-          Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
-        },
-      });
-      const json = await response.json();
-      if (!json.data) throw json;
-
-      followers.push(...json.data);
-
-      if (json.meta.next_token)
-        return await getFollowersSet(json.meta.next_token);
-      return console.log("done getting followers");
-    } catch (err) {
-      return console.log(err);
-    }
-  }
-
-  await getFollowersSet();
-
-  if (followers.length)
-    writeFile("./followers.json", JSON.stringify(followers), (err) =>
-      console.error(err)
-    );
+if (!process.env.TWITTER_BEARER_TOKEN) {
+  throw new Error("TWITTER_BEARER_TOKEN is not set");
 }
 
-export default async function main() {
-  await getFollowers();
+const twitterClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN).v2
+  .readOnly;
 
-  return console.log("done");
+async function getFollowers() {
+  if (!process.env.TWITTER_USER_ID) {
+    throw new Error("TWITTER_USER_ID is not set");
+  }
+
+  /** @type import("twitter-api-v2").UserV2[] */
+  const followers = [];
+  /** @type import("twitter-api-v2").UserV2TimelineResult */
+  let followersRes = {
+    data: [],
+    meta: { result_count: 100, next_token: undefined },
+  };
+
+  while (followersRes.meta.next_token) {
+    followersRes = await twitterClient.followers(
+      process.env.TWITTER_USER_ID,
+      followersRes.meta.next_token !== undefined
+        ? {}
+        : { pagination_token: followersRes.meta.next_token }
+    );
+    followersRes.data.forEach((user) => {
+      followers.push(user);
+    });
+  }
+
+  return followers;
+}
+
+async function getFollowing() {
+  if (!process.env.TWITTER_USER_ID) {
+    throw new Error("TWITTER_USER_ID is not set");
+  }
+
+  /** @type import("twitter-api-v2").UserV2[] */
+  const following = [];
+  /** @type import("twitter-api-v2").UserV2TimelineResult */
+  let followingRes = {
+    data: [],
+    meta: { result_count: 100, next_token: undefined },
+  };
+
+  while (followingRes.meta.next_token) {
+    followingRes = await twitterClient.following(
+      process.env.TWITTER_USER_ID,
+      followingRes.meta.next_token !== undefined
+        ? {}
+        : { pagination_token: followingRes.meta.next_token }
+    );
+    followingRes.data.forEach((user) => {
+      following.push(user);
+    });
+  }
+
+  return following;
+}
+
+async function main() {
+  const following = await getFollowing();
+  await writeFile("./following.json", JSON.stringify(following, null, 2));
+
+  const followers = await getFollowers();
+  await writeFile("./followers.json", JSON.stringify(followers, null, 2));
 }
 
 main();
