747c3698b7a6236a7d4ef3631842826260b3d99d
Pascal Pixel <x@pascalpixel.com>
2022-08-21 17:39:45 +0100

    convert scripts to their own files

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 import { config as loadEnvVariables } from "dotenv";
-import { readdir, readFile, writeFile } from "fs/promises";
+import { writeFile } from "fs/promises";
 import { TwitterApi } from "twitter-api-v2";
 
 loadEnvVariables();
@@ -105,62 +105,9 @@ async function getDataForCurrent() {
   );
 }
 
-async function compareUsers(type = "followers") {
-  // get all file dates
-  const fileNames = await readdir("./cache");
-  const usersFileNames = fileNames.filter((fileName) =>
-    fileName.includes(`${type}-`)
-  );
-  const usersFileDates = usersFileNames.map(
-    (fileName) => fileName.split(`${type}-`)[1].split(".")[0]
-  );
-  const usersFileDatesSorted = usersFileDates.sort(
-    (a, b) => new Date(a).getTime() - new Date(b).getTime()
-  );
-
-  // Get most recent file
-  const mostRecentFileDate =
-    usersFileDatesSorted[usersFileDatesSorted.length - 1];
-  const mostRecentFile = `./cache/${type}-${mostRecentFileDate}.json`;
-  const mostRecentFileData = await readFile(mostRecentFile, "utf-8");
-  /** @type import("twitter-api-v2").UserV2[] */
-  const usersCurrent = JSON.parse(mostRecentFileData);
-
-  // Get second most recent file
-  const secondMostRecentFileDate =
-    usersFileDatesSorted[usersFileDatesSorted.length - 2];
-  const secondMostRecentFile = `./cache/${type}-${secondMostRecentFileDate}.json`;
-  const secondMostRecentFileData = await readFile(
-    secondMostRecentFile,
-    "utf-8"
-  );
-  /** @type import("twitter-api-v2").UserV2[] */
-  const usersPrev = JSON.parse(secondMostRecentFileData);
-
-  // Find usersPrev not in usersCurrent
-  const lost = usersPrev.filter((user) => {
-    return !usersCurrent.some((userCurrent) => user.id === userCurrent.id);
-  });
-  console.log(
-    `Lost ${lost.length} users`,
-    lost.map((user) => user.username)
-  );
-
-  // Find usersCurrent not in usersPrev
-  const gained = usersCurrent.filter((user) => {
-    return !usersPrev.some((userPrev) => user.id === userPrev.id);
-  });
-  console.log(
-    `Gained ${gained.length} users`,
-    gained.map((user) => user.username)
-  );
-}
-
 async function main() {
   console.log("Starting...");
   await getDataForCurrent();
-  await compareUsers("followers");
-  await compareUsers("following");
   console.log("Done!");
 }
 
