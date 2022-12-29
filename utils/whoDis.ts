8f5b1586187e5e4bd68d7fa0bfa7d65f940b6748
Pascal Pixel <x@pascalpixel.com>
2022-12-29 01:06:08 +0100

    whoDis

--- utils/whoDis.ts
+++ utils/whoDis.ts
@@ -3,135 +3,104 @@ import { getFollowers } from "./getFollowers";
 export default async function whoDis() {
   const allFollowers = await getFollowers();
 
-  const goodFollowers = [...allFollowers]
-    .filter(
-      // following me 10k+ followers
-      (follower) => (follower.public_metrics?.followers_count || 0) > 10000
-    )
-    .filter(
-      // Remove if they follow more than 2000 people
-      (follower) => (follower.public_metrics?.following_count || 0) < 2000
-    )
-    .filter(
-      // remove if their ratio is less than 1
-      (follower) =>
-        Math.round(
-          (follower.public_metrics?.followers_count || 0) /
-            (follower.public_metrics?.following_count || 1)
-        ) > 5
-    )
+  const bigFish = [...allFollowers]
     .sort((a, b) => {
       // sort by followers
-      const aFollowers = a.public_metrics?.followers_count || 0;
-      const bFollowers = b.public_metrics?.followers_count || 0;
-      return bFollowers - aFollowers;
-    });
-
-  const otherFollowers = [...allFollowers]
-    .filter((follower) => {
-      // not in good followers
-      return !goodFollowers.includes(follower);
-    })
-    .filter(
-      // following me 10k+ followers
-      (follower) => (follower.public_metrics?.followers_count || 0) > 10000
-    )
-    .filter(
-      // Remove if they follow more than 5000 people
-      (follower) => (follower.public_metrics?.following_count || 0) < 5000
-    )
-    .filter(
-      // remove if their ratio is less than 1
-      (follower) =>
-        Math.round(
-          (follower.public_metrics?.followers_count || 0) /
-            (follower.public_metrics?.following_count || 1)
-        ) > 5
-    )
-    .sort((a, b) => {
-      // sort by followers
-      const aFollowers = a.public_metrics?.followers_count || 0;
-      const bFollowers = b.public_metrics?.followers_count || 0;
-      return bFollowers - aFollowers;
-    });
-
-  const restFollowers = [...allFollowers]
-    .filter((follower) => {
-      // not in good followers
-      return !goodFollowers.includes(follower);
-    })
-    .filter((follower) => {
-      // not in other followers
-      return !otherFollowers.includes(follower);
-    })
-    .filter(
-      // following me 10k+ followers
-      (follower) => (follower.public_metrics?.followers_count || 0) > 1000
-    )
-    .filter(
-      // Remove if they follow more than 5000 people
-      (follower) => (follower.public_metrics?.following_count || 0) < 2000
-    )
-    .filter(
-      // remove if their ratio is less than 1
-      (follower) =>
-        Math.round(
-          (follower.public_metrics?.followers_count || 0) /
-            (follower.public_metrics?.following_count || 1)
-        ) > 5
-    )
-    .sort((a, b) => {
-      // sort by followers
-      const aFollowers = a.public_metrics?.followers_count || 0;
-      const bFollowers = b.public_metrics?.followers_count || 0;
-      return bFollowers - aFollowers;
-    });
-
-  const whomeverLeftFollowers = [...allFollowers]
-    .filter((follower) => {
-      // not in good followers
-      return !goodFollowers.includes(follower);
-    })
-    .filter((follower) => {
-      // not in other followers
-      return !otherFollowers.includes(follower);
-    })
-    .filter((follower) => {
-      // not in rest followers
-      return !restFollowers.includes(follower);
-    })
-    .filter(
-      // following me 10k+ followers
-      (follower) => (follower.public_metrics?.followers_count || 0) > 10000
-    )
-    .filter(
-      // remove if their ratio is less than 1
-      (follower) =>
-        Math.round(
-          (follower.public_metrics?.followers_count || 0) /
-            (follower.public_metrics?.following_count || 1)
-        ) > 5
-    )
-    .sort((a, b) => {
-      // sort by followers
-      const aFollowers = a.public_metrics?.followers_count || 0;
-      const bFollowers = b.public_metrics?.followers_count || 0;
-      return bFollowers - aFollowers;
-    });
-
-  [goodFollowers, otherFollowers, restFollowers, whomeverLeftFollowers].forEach(
-    (followers) => {
-      console.table(
-        followers.map((follower) => [
-          follower.username,
-          follower.public_metrics?.followers_count,
-          follower.public_metrics?.following_count,
-          Math.round(
-            (follower.public_metrics?.followers_count || 0) /
-              (follower.public_metrics?.following_count || 1)
-          ),
-        ])
+      return (
+        (b.public_metrics?.followers_count || 0) -
+        (a.public_metrics?.followers_count || 0)
       );
-    }
-  );
+    })
+    .slice(0, 10);
+
+  const greatRatio = [...allFollowers]
+    .filter(
+      // has a large following
+      (follower) => (follower.public_metrics?.followers_count || 0) > 10000
+    )
+    .filter(
+      // pays attention to whom they follow
+      (follower) => (follower.public_metrics?.following_count || 0) < 2000
+    )
+    .filter(
+      // stellar ratio
+      (follower) =>
+        Math.round(
+          (follower.public_metrics?.followers_count || 0) /
+            (follower.public_metrics?.following_count || 1)
+        ) > 100
+    )
+    .sort((a, b) => {
+      // sort by followers
+      return (
+        (b.public_metrics?.followers_count || 0) -
+        (a.public_metrics?.followers_count || 0)
+      );
+    });
+
+  const greatPeeps = [...allFollowers]
+    .filter(
+      // has a decent following
+      (follower) => (follower.public_metrics?.followers_count || 0) > 10000
+    )
+    .filter(
+      // pays attention to whom they follow
+      (follower) => (follower.public_metrics?.following_count || 0) < 2000
+    )
+    .filter(
+      // remove bad ratio
+      (follower) =>
+        Math.round(
+          (follower.public_metrics?.followers_count || 0) /
+            (follower.public_metrics?.following_count || 1)
+        ) > 20
+    )
+    .sort((a, b) => {
+      // sort by followers
+      return (
+        (b.public_metrics?.followers_count || 0) -
+        (a.public_metrics?.followers_count || 0)
+      );
+    });
+
+  const goodPeeps = [...allFollowers]
+    .filter(
+      // has a decent following
+      (follower) => (follower.public_metrics?.followers_count || 0) > 10000
+    )
+    .filter(
+      // pays attention to whom they follow
+      (follower) => (follower.public_metrics?.following_count || 0) < 2000
+    )
+    .filter(
+      // remove bad ratio
+      (follower) =>
+        Math.round(
+          (follower.public_metrics?.followers_count || 0) /
+            (follower.public_metrics?.following_count || 1)
+        ) > 5
+    )
+    .sort((a, b) => {
+      // sort by followers
+      return (
+        (b.public_metrics?.followers_count || 0) -
+        (a.public_metrics?.followers_count || 0)
+      );
+    });
+
+  const groups = [bigFish, greatRatio, greatPeeps, goodPeeps];
+
+  groups.forEach((followers) => {
+    console.table(
+      followers.map((follower) => [
+        follower.username,
+        follower.public_metrics?.followers_count,
+        follower.public_metrics?.following_count,
+        Math.round(
+          (follower.public_metrics?.followers_count || 0) /
+            (follower.public_metrics?.following_count || 1)
+        ),
+      ])
+    );
+  });
 }
